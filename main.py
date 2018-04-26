e25e4a31187f667e090a987e600649cb5d82ccca
B.frolikov <b.frolikov@gmail.com>
2018-04-26 18:59:00 +0300

    now i can see center

--- main.py
+++ main.py
@@ -2,36 +2,57 @@ import cv2
 import numpy as np
 img = cv2.imread('img.jpg')
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
 lower_red1 = np.array([0,0,0])
-upper_red1 = np.array([15,255,255])
+upper_red1 = np.array([0,0,0])
 
-lower_red2 = np.array([150,0,0])
-upper_red2 = np.array([180,255,255])
+lower_red2 = np.array([170, 170, 100])
+upper_red2 = np.array([180, 255, 255])
 
-mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
-mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+global hsv
 
-mask = cv2.bitwise_or(mask1, mask2)
+cap = cv2.VideoCapture(0)
 
-mask = cv2.erode(mask, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5)))
-mask = cv2.dilate(mask, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5)))
+def onclick(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN : 
+        global hsv
+        pixel = hsv[y,x]
+        print("{} {}".format((x, y), pixel))
 
-cv2.imshow('1', mask)
+cv2.namedWindow('frame')
+cv2.setMouseCallback('frame', onclick)
 
-mask = cv2.dilate(mask, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5)))
-mask = cv2.erode(mask, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5)))
 
-cv2.imshow('2', mask)
 
-cv2.waitKey(0)
+while True:
+    _, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+    mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+
+    mask = cv2.bitwise_or(mask1, mask2)
+
+    mask = cv2.erode(mask, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5)))
+    mask = cv2.dilate(mask, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5)))
+
+    mask = cv2.dilate(mask, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5)))
+    mask = cv2.erode(mask, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5)))
+
+    res = cv2.bitwise_and(frame, frame, mask = mask)
+
+    x, y = (mask > 0).nonzero()
+    if len(x) > 0 :
+        mx = int(sum(x)/len(x))
+        my = int(sum(y)/len(y))
+        cv2.circle(frame, (my, mx), 4, 255, 3)
+
+
+    cv2.imshow('frame', frame)
+    cv2.imshow('res', res)
+    cv2.imshow('mask', mask)
+
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27 : break
+
 cv2.destroyAllWindows()
-
-
-res = cv2.bitwise_and(img, img, mask = mask)
-
-cv2.imshow('a', res)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cap.release()
